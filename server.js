5c0ba75a92595f1c2d8d6f1cff2afc2355fa660e
laurenmarsh <laurenmdb@gmail.com>
2024-01-05 13:32:35 -0500

    cors options for cors issue on frontend not allowed No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ import router from './routes/routes.js';
 const app = express();
 const PORT = process.env.PORT || 7008;
 
+const corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(express.json());
 app.use(cors());
 
 app.use(express.static('views'));
-app.use('/lila', router);
+app.use('/lila', cors(corsOptions),router);
 
 app.listen(PORT, () => {
     console.log('lila is running on port 7008');
