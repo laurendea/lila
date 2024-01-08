eba53b01c4519fa1f55a06f928c5f8e0401dceb0
laurenmarsh <laurenmdb@gmail.com>
2024-01-07 20:58:00 -0500

    changed post gratitude route

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import multer from 'multer';
+import path from 'path';
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'upload/images' });
+const fs = require('fs').promises;
 
 import { createMeditationEntry, getMeditationEntries, updateMeditationEntry, deleteMeditationEntry, getGratitudeEntries, createGratitudeEntry, updateGratitudeEntry, deleteGratitudeEntry } from '../controllers/controllers.js';
 
@@ -14,7 +16,44 @@ import { createMeditationEntry, getMeditationEntries, updateMeditationEntry, del
 router.get('/gratitude-entries', getGratitudeEntries);
 
 // Route for creating a new meditation entry
-router.post('/create-gratitude-entry', upload.single('photo'), createGratitudeEntry);
+router.post("/create-gratitude-entry", upload.single('photo'), createGratitudeEntry, async (req, res) => {
+    try {
+      // Extract form data
+      const { date, entry } = req.body;
+  
+      // Process the uploaded file
+      if (req.file) {
+        const photoFile = req.file;
+  
+        // Move the file to the desired destination
+        const filePath = path.join(__dirname, 'upload/images', photoFile.filename);
+        await fs.promises.rename(photoFile.path, filePath);
+  
+        // Your file processing logic here
+        console.log('File details:', {
+          originalname: photoFile.originalname,
+          filename: photoFile.filename,
+          mimetype: photoFile.mimetype,
+          size: photoFile.size,
+          destination: filePath,
+        });
+  
+        // Save the gratitude entry to the database
+        const newGratitudeEntry = new newGratitudeEntry({
+          date: new Date(date),
+          entry,
+          photoUrl: `http://localhost:7008/photo/${photoFile.filename}`
+        });
+  
+        await newGratitudeEntry.save();
+      }
+  
+      res.status(201).json({ message: 'Gratitude entry created successfully' });
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
 
 // Route for updating a meditation entry by date
 router.put('/update-gratitude-entry/:date', updateGratitudeEntry);
